0f22ed3fbbbc9c1cc8a6bfcdcfa04e524808ec24
John <shu.peng@coscico.com>
2014-07-29 14:59:57 +0800

    1. Add fmdb 2. Setup AppDelegate.m 3. Add .gitignore

---
+++ Chat/Chat/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Chat
+//
+//  Created by shupeng on 14-7-29.
+//  Copyright (c) 2014年 John. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Chat/Chat/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Chat
+//
+//  Created by shupeng on 14-7-29.
+//  Copyright (c) 2014年 John. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
